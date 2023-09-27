679965b660887f5b5e6852130b79ef16c9aa010e
adxJiaojiao <56509338+adxJiaojiao@users.noreply.github.com>
2023-09-27 11:30:03 +0800

    Update pdd580ua.js

--- pdd580ua.js
+++ pdd580ua.js
@@ -1,3 +1,3 @@
 const userAgent = $request.headers['User-Agent'];
-const modifiedUserAgent = userAgent.replace(/(com\.xunmeng\.pinduoduo AppVersion)\/[0-9.]+ (AppBuild)\/[0-9]+ (pversion)\/[0-9]+/, '$1/5.8.1 $2/2004100017 $3');
+const modifiedUserAgent = userAgent.replace(/iPhone.+?Mobile/, 'Mozilla/5.0 (iPhone; CPU iPhone OS 16_6_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 iOS/16.6.1 Model/iPhone14,2 BundleID/com.xunmeng.pinduoduo AppVersion/5.8.1 AppBuild/2004100017 pversion/0');
 $done({ headers: { ...$request.headers, 'User-Agent': modifiedUserAgent } });
