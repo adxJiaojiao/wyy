e06c77ae4a714a7e277ff56cfa31e02cbe4008d0
adxJiaojiao <56509338+adxJiaojiao@users.noreply.github.com>
2023-09-27 10:35:51 +0800

    Update pdd580ua.js

--- pdd580ua.js
+++ pdd580ua.js
@@ -1,2 +1,3 @@
 const userAgent = $request.headers['User-Agent'];
-const modifiedUserAgent = userAgent.replace(/(com\.xunmeng\.pinduoduo AppVersion)\/[0-9.]+ (AppBuild)
+const modifiedUserAgent = userAgent.replace(/(com\.xunmeng\.pinduoduo AppVersion)\/[0-9.]+ (AppBuild)\/[0-9]+ (pversion)\/[0-9]+/, '$1/5.8.1 $2/2004100017 $3');
+$done({ headers: { ...$request.headers, 'User-Agent': modifiedUserAgent } });
